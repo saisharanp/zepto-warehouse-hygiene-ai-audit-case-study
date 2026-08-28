34d06f89c8f87ade4dda98dc4d50e46ab545a0e5
Saisharan P <saisharan.p@yahoo.com>
2026-08-28 16:13:42 +0530

    chore: move desktop cat to Pounce repository

--- desktop-cat/Sources/DesktopCatCore/Domain/CatActivity.swift
+++
@@ -1,4 +0,0 @@
-public enum CatActivity: String, CaseIterable, Codable {
-    case sitting, loafing, walking, sleeping, waking, stretching, grooming
-    case kneading, lookingAround, pouncing, zooming, hiding, peeking, eating, sunbathing
-}
